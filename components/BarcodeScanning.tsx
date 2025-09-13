9dfbf4fe080deba2afe22111bba071125059273e
Zeyd SAIAD <123203352+zeeeeeyd@users.noreply.github.com>
2025-09-13 20:10:30 +0200

    Added package-lock.json

--- components/BarcodeScanning.tsx
+++ components/BarcodeScanning.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { CameraView, CameraType } from 'expo-camera';
 
 interface BarcodeScanningProps {
